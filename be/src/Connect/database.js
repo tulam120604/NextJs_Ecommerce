39c8e17b6742ba86202d85bf8f2fe4bc2d08e22c
tulam120604 <saudoi1204@gmail.com>
2024-11-25 23:03:04 +0700

    update version library

--- be/src/Connect/database.js
+++ be/src/Connect/database.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-export default async function ConnectDB (uri) {
+export default async function ConnectDB(uri) {
     try {
-         await mongoose.connect(uri);
+        await mongoose.connect(uri);
         console.log(`Connect Success  to uri : ${uri}`);
     }
     catch {
